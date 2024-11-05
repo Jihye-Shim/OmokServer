c6af4c04cb50d6f3a42c22ec43799db4e3ab2cd9
Jihye-Shim <81310697+Jihye-Shim@users.noreply.github.com>
2024-11-05 19:25:02 +0900

    feat: Add address output

--- Omok/ServerCore/SocketAddress.h
+++ Omok/ServerCore/SocketAddress.h
@@ -1,4 +1,5 @@
 #pragma once
+#pragma warning(disable:4996)
 using SocketAddressRef = shared_ptr<class SocketAddress>;
 
 class SocketAddress
@@ -9,6 +10,12 @@ public:
 	SocketAddress(wstring ip, int32 port);
 
 	SOCKADDR_IN& GetSockAddr() { return _sockAddr; }
+	string GetAdddr() {
+		string str = inet_ntoa(_sockAddr.sin_addr);
+		str.append(":");
+		str.append(to_string(ntohs(_sockAddr.sin_port)));
+		return str;
+	}
 private:
 	SOCKADDR_IN _sockAddr = {};
 };
